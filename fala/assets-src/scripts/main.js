d165dfb63a7047c871b4712cc5dc251451b6748d
Tyom Semonov <inspiro@gmail.com>
2015-10-09 16:23:28 +0100

    Use Mojular JS modules

--- fala/assets-src/scripts/main.js
+++ fala/assets-src/scripts/main.js
@@ -1,3 +1,7 @@
 require('mojular');
+
+require('mojular-moj-elements/assets/scripts/modules/skip-to-content');
+require('mojular-moj-elements/assets/scripts/modules/devs');
+require('mojular-moj-elements/assets/scripts/modules/cookie-message');
+
 require('./find-legal-adviser');
-require('./skip-to-content');
--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,19 @@ module.exports = {
     path: 'fala/assets/scripts',
     filename: '[name].bundle.js'
   },
+  module: {
+    loaders: [
+      { include: /\.json$/, loaders: ['json-loader'] }
+    ]
+  },
   resolve: {
     modulesDirectories: [
       'node_modules',
       'node_modules/mojular/node_modules'
-    ]
-  }
+    ],
+    extensions: ['', '.json', '.js']
+  },
+  plugins: [
+    new webpack.optimize.DedupePlugin()
+  ]
 };
