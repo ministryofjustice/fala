348263626931a2b05ec69bbfdf4719c4b438052a
Tyom Semonov <inspiro@gmail.com>
2015-10-12 17:19:17 +0100

    Add Google Analytics

--- fala/settings/base.py
+++ fala/settings/base.py
@@ -76,6 +76,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 # 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'adviser.context_processors.ga_id',
             ],
         },
     },
