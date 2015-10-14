e9db94b0192c1a73605e081e9c50ec6073e86280
Tyom Semonov <inspiro@gmail.com>
2015-10-14 10:53:12 +0100

    Move static serve to main `urls.py`

--- fala/apps/adviser/urls.py
+++ fala/apps/adviser/urls.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-from django.conf import settings
-from django.conf.urls.static import static
 from django.conf.urls import url
 from .views import LocationSearchView, OrganisationSearchView
 
@@ -8,10 +6,4 @@ urlpatterns = [
     url(r'^$', LocationSearchView.as_view(), name='root'),
     url(r'^location/$', LocationSearchView.as_view(), name='location'),
     url(r'^organisation/$', OrganisationSearchView.as_view(), name='organisation'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
-if settings.DEBUG_STATIC:
-    urlpatterns += [
-        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    ]
+]
--- fala/urls.py
+++ fala/urls.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^', include('adviser.urls')),
-]
+    url(r'^', include('feedback.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG_STATIC:
+    urlpatterns += [
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    ]
