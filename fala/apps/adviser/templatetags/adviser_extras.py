084276cb7c248e022dec7610b4a677c60f7868a8
Tyom Semonov <inspiro@gmail.com>
2015-10-14 17:09:22 +0100

    Use custom `url_to_human` and `human_to_url` filters

--- fala/apps/adviser/templatetags/adviser_extras.py
+++ fala/apps/adviser/templatetags/adviser_extras.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
 
+import re
+from urllib.parse import urlparse, parse_qs, quote
 from django_jinja import library
 from jinja2 import contextfilter
-from urllib.parse import urlparse, parse_qs
 
 
 @library.filter
-def multiply(value, arg):
-    return value * arg
+def url_to_human(value):
+    return re.sub(r'(^https?://)|(/$)', '', value)
+
 
 @library.filter
-def val(field):
-    return field.value() if field.value() else ''
+def human_to_url(value):
+    return re.sub(r'^((?!https?://).*)', r'http://\1', quote(value))
+
 
 @library.filter
 @contextfilter
@@ -26,4 +29,3 @@ def query_to_dict(context, value, prop=None):
 @contextfilter
 def to_fala_page_url(context, value, url_path):
     return '%s?%s' % (url_path, urlparse(value).query)
-
