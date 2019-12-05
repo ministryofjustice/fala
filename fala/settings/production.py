628586a00ffa6eca011be34608091806200131c1
said-moj <45761276+said-moj@users.noreply.github.com>
2019-12-05 15:39:21 +0000

    Make LAALAA_API_HOST setting required on production environment
    Co-authored-by: Dávid Lantos <sldblog@users.noreply.github.com>

--- fala/settings/production.py
+++ fala/settings/production.py
@@ -1,7 +1,13 @@
 import os
 from .base import *  # noqa: F401,F403
 
-settings_required = ("SECRET_KEY", "ZENDESK_API_USERNAME", "ZENDESK_API_TOKEN", "GOOGLE_MAPS_API_KEY")
+settings_required = (
+    "SECRET_KEY",
+    "ZENDESK_API_USERNAME",
+    "ZENDESK_API_TOKEN",
+    "GOOGLE_MAPS_API_KEY",
+    "LAALAA_API_HOST",
+)
 
 for key in settings_required:
     if key not in os.environ:
