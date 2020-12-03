ccad1c249a1815de5fa6b8e65aae1c86e3565a7c
Malcolm Butler <32877315+MoJ-Longbeard@users.noreply.github.com>
2020-12-03 12:58:39 +0000

    LGA-1520 Accessibility Statement

--- fala/urls.py
+++ fala/urls.py
@@ -2,12 +2,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
-from adviser.views import AdviserView
+from adviser.views import AdviserView, AccessibilityView
 from django.views.static import serve
 
-urlpatterns = [url(r"^$", AdviserView.as_view(), name="adviser")] + static(
-    settings.STATIC_URL, document_root=settings.STATIC_ROOT
-)
+urlpatterns = [
+    url(r"^information$", AccessibilityView.as_view(), name="information"),
+    url(r"^$", AdviserView.as_view(), name="adviser"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG_STATIC:
     urlpatterns += [url(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT})]
