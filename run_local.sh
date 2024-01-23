3d3fba654eb22549b4b11e6badcf00d02dc92a00
said-moj <45761276+said-moj@users.noreply.github.com>
2024-01-23 16:32:19 +0000

    Collect static asset after creating webapp container

--- run_local.sh
+++ run_local.sh
@@ -3,4 +3,6 @@ if [ ! -f fala/settings/local.py ]; then
   cp fala/settings/local.example.py fala/settings/local.py
 fi
 
-docker-compose up --build
+docker-compose up -d --build
+docker-compose run webapp python3 manage.py collectstatic --no-input
+docker-compose logs -f webapp
