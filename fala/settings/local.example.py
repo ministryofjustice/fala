99022e016cf106c956f917521c85b6154fee1902
Stephen Dicks <github@starswan.com>
2024-03-19 09:19:37 +0000

    black reformat

--- fala/settings/local.example.py
+++ fala/settings/local.example.py
@@ -4,5 +4,7 @@ import os
 DEBUG = True
 ALLOWED_HOSTS = ["localhost"]
 DEBUG_STATIC = True
-LAALAA_API_HOST = os.environ.get("LAALAA_API_HOST", "https://laa-legal-adviser-api-staging.apps.live-1.cloud-platform.service.justice.gov.uk")
+LAALAA_API_HOST = os.environ.get(
+    "LAALAA_API_HOST", "https://laa-legal-adviser-api-staging.apps.live-1.cloud-platform.service.justice.gov.uk"
+)
 ENVIRONMENT = "dev"
