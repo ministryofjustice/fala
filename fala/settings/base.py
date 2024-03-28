120e67bb2ef3f3bc42c44ded553cadfcfe336d02
psweeting <patrick.sweeting@digital.justice.gov.uk>
2024-03-28 13:55:52 +0000

    Testing

--- fala/settings/base.py
+++ fala/settings/base.py
@@ -215,7 +215,7 @@ if "SENTRY_DSN" in os.environ:
 
 LAALAA_API_HOST = os.environ.get("LAALAA_API_HOST", None)
 
-FALA_NO_MAP_SETTING = bool(os.environ.get("FALA_NO_MAP_ENABLED", ""))
+FALA_NO_MAP_SETTING = os.environ.get("FALA_NO_MAP_ENABLED", "").lower() == "true"
 
 ENVIRONMENT = os.environ.get("ENVIRONMENT", "unknown")
 
