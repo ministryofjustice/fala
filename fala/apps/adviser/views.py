6202cbb7739aeedd42f0701183580bf48a0647b2
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2024-04-11 16:04:11 +0100

    update views.py

--- fala/apps/adviser/views.py
+++ fala/apps/adviser/views.py
@@ -26,7 +26,6 @@ class AdviserView(TemplateView):
                 "current_url": current_url,
                 "GOOGLE_MAPS_API_KEY": settings.GOOGLE_MAPS_API_KEY,
                 "LAALAA_API_HOST": settings.LAALAA_API_HOST,
-                "FEATURE_FLAG_NO_MAP": settings.FEATURE_FLAG_NO_MAP,
                 "CHECK_LEGAL_AID_URL": settings.CHECK_LEGAL_AID_URL,
             }
         )
