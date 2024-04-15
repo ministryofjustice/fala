d29fa79515aac9254119ad37b35e0bf44835a635
Stephen Dicks <github@starswan.com>
2024-04-15 12:24:04 +0100

    lint

--- fala/apps/adviser/tests/test_search_view_function.py
+++ fala/apps/adviser/tests/test_search_view_function.py
@@ -5,8 +5,8 @@ from django.urls import reverse
 class SearchViewFunctionTest(SimpleTestCase):
     client = Client()
 
-    url = reverse('adviser')
+    url = reverse("adviser")
 
     def test_postcode_search(self):
-        response = self.client.get(self.url, { 'postcode': 'PE31' })
+        response = self.client.get(self.url, {"postcode": "PE31"})
         self.assertEqual(response.status_code, 200)
