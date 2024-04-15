3fbeb3d52b3739ceb10e7efdd938d59506da871b
Stephen Dicks <github@starswan.com>
2024-04-15 13:17:04 +0100

    new test

--- fala/apps/adviser/tests/test_search_view_function.py
+++ fala/apps/adviser/tests/test_search_view_function.py
@@ -10,3 +10,7 @@ class SearchViewFunctionTest(SimpleTestCase):
     def test_postcode_search(self):
         response = self.client.get(self.url, {"postcode": "PE31"})
         self.assertEqual(response.status_code, 200)
+
+    def test_postcode_search_zero_results(self):
+        response = self.client.get(self.url, {"postcode": "ZZZ"})
+        self.assertContains(response, "There are no results matching your search criteria")
