e8bc7934f34f51b0e2fa6d5a0593f58b8a0302ac
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2024-04-16 16:27:36 +0100

    update tests

--- fala/apps/adviser/tests/test_search_view_function.py
+++ fala/apps/adviser/tests/test_search_view_function.py
@@ -1,7 +1,8 @@
-from django.test import SimpleTestCase, Client
+from django.test import SimpleTestCase, Client, override_settings
 from django.urls import reverse
 
 
+@override_settings(FEATURE_FLAG_NO_MAP=True)
 class SearchViewFunctionTest(SimpleTestCase):
     client = Client()
 
@@ -16,14 +17,20 @@ class SearchViewFunctionTest(SimpleTestCase):
         self.assertEqual({"postcode": ["Postcode not found"]}, response.context_data["form"].errors)
 
 
+@override_settings(FEATURE_FLAG_NO_MAP=True)
 class NewSearchViewTemplate(SimpleTestCase):
     client = Client()
 
     def test_url_link_is_in_DOM(self):
         response = self.client.get(reverse("adviser"))
         self.assertEqual(response.status_code, 200)
+
+        # Ensure URL link is in DOM
         self.assertContains(
             response,
-            '<a href="https://www.gov.uk/check-legal-aid">Check if you qualify for legal aid (opens in a new tab)</a>',
+            '<a class="govuk-link" href="https://www.gov.uk/check-legal-aid" target="_blank" rel="noopener">Check if you qualify for legal aid (opens in a new tab)</a>',
+            html=True,
         )
-        self.assertTemplateUsed(response, "search.html")
+
+        # Ensure CSS class is visible in DOM
+        self.assertContains(response, '<div class="laa-fala__grey-box">')
