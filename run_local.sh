8410106f3ea495ddda39e667eb3fdc4958b57615
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2024-05-01 10:30:56 +0100

    update where `.env` lives

--- run_local.sh
+++ run_local.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-if [ ! -f fala/settings/.env ]; then
-  cp fala/settings/.env.example fala/settings/.env
+if [ ! -f .env ]; then
+  cp .env.example .env
 fi
 
 docker-compose up -d --build
 docker-compose run webapp python3 manage.py collectstatic --no-input
 docker-compose logs -f webapp
+
+
