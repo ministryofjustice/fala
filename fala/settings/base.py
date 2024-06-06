f4416fa8298a518ef7382dd49cad4f5aa996793e
Hettie Street <hettie.street@digital.justice.gov.uk>
2024-06-06 16:36:50 +0100

    NO-TICKET: DB rename and tidy-up

--- fala/settings/base.py
+++ fala/settings/base.py
@@ -36,7 +36,7 @@ MANAGERS = ADMINS
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME", "fala_db"),
+        "NAME": os.environ.get("DB_NAME", "fala_development"),
         "USER": os.environ.get("DB_USER", ""),
         "PASSWORD": os.environ.get("DB_PASSWORD", "postgres"),
         "PORT": os.environ.get("DB_PORT", "5432"),
--- run_local.sh
+++ run_local.sh
@@ -5,6 +5,7 @@ fi
 
 docker-compose up -d --build
 docker-compose run webapp python3 manage.py collectstatic --no-input
+docker-compose exec webapp python3 manage.py migrate
 docker-compose logs -f webapp
 
 
