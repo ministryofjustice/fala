9aa3f1bef68c7c88388fc1a73a8326fd63a2517a
Stephen Dicks <github@starswan.com>
2024-06-06 08:06:00 +0100

    EL-1490 move assets from S3 into being served from target

--- fala/urls.py
+++ fala/urls.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 from django.conf import settings
-from django.conf.urls.static import static
+
 from django.urls import re_path
 from adviser.views import AccessibilityView, AdviserView, SearchView
 from django.views.static import serve
@@ -9,7 +9,4 @@ urlpatterns = [
     re_path(r"^accessibility-statement$", AccessibilityView.as_view(), name="accessibility_statement"),
     re_path(r"^$", AdviserView.as_view(), name="adviser"),
     re_path(r"^search$", SearchView.as_view(), name="search"),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-if settings.DEBUG_STATIC:
-    urlpatterns += [re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT})]
+] + [re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT})]
