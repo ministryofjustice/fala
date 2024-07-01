5cb5d2a57de3c74f744a647ee98131532c74d9c8
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2024-07-01 10:16:27 +0100

    EL-1489: Remove modular & use GOVUK frontend (#283)
    * update gulp
    
    * review commments
    - add styling typography stylings
    - add `govuk-frontend-supported` to use js
    - add custom css, not initially moved across
    
    * EL-1489 Updating warning text component to new GDS styling
    
    ---------
    
    Co-authored-by: psweeting <patrick.sweeting@digital.justice.gov.uk>

--- fala/assets-src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-var Mojular = require('mojular');
-
-Mojular
-  .use([
-    require('mojular-govuk-elements'),
-    require('mojular-moj-elements/modules/devs'),
-    require('./find-legal-adviser')
-  ])
-  .init();
