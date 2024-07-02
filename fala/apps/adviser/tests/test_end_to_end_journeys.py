1848012022765beb6c32e15009c871611eddaf2e
Hettie Street <hettie.street@digital.justice.gov.uk>
2024-07-02 09:19:30 +0100

    Addfeature flag for circle config

--- fala/apps/adviser/tests/test_end_to_end_journeys.py
+++ fala/apps/adviser/tests/test_end_to_end_journeys.py
@@ -1,8 +1,9 @@
 from playwright.sync_api import expect
 from fala.apps.adviser.tests.playwright.setup import PlaywrightTestSetup
-from time import sleep
+from django.test import override_settings
 
 
+@override_settings(FEATURE_FLAG_NO_MAP=True)
 class TestEndToEndJourneys(PlaywrightTestSetup):
     def test_landing_page(self):
         page = self.browser.new_page()
@@ -15,7 +16,6 @@ class TestEndToEndJourneys(PlaywrightTestSetup):
         page.goto(f"{self.live_server_url}")
         page.get_by_label("Postcode").fill("SE11")
         page.get_by_role("button", name="Search").click()
-        sleep(10)
         expect(page.locator("h1")).to_have_text("Search results")
         expect(page.get_by_role("listitem").filter(has_text="Postcode: SE11")).to_be_visible()
         expect(page.get_by_text("in order of closeness")).to_be_visible()
