dc90f01913667937d63fee504744331f718f2934
Stephen Dicks <github@starswan.com>
2024-07-11 17:28:36 +0100

    Revert "EL-1453 Replace gulp with django-webpack-loader (#296)"
    This reverts commit 76589cefc0f91d97c15bcafabf1a58f6c45c9451.

--- fala/urls.py
+++ fala/urls.py
@@ -10,8 +10,4 @@ urlpatterns = [
     re_path(r"^privacy$", PrivacyView.as_view(), name="privacy"),
     re_path(r"^$", AdviserView.as_view(), name="adviser"),
     re_path(r"^search$", SearchView.as_view(), name="search"),
-    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
-    # serve govuk assets directly from the location they were built into
-    # as recommended by https://frontend.design-system.service.gov.uk/import-font-and-images-assets/
-    re_path(r"^assets/(?P<path>.*)$", serve, {"document_root": settings.GOVUK_ASSETS_ROOT}),
-]
+] + [re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT})]
