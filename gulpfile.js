db004ad5035184f9bc9020815f09987454b2d136
Stephen Dicks <github@starswan.com>
2024-07-11 17:41:10 +0100

    Revert "EL-1453 Replace gulp with django-webpack-loader (#296)" (#311)
    This reverts commit 76589cefc0f91d97c15bcafabf1a58f6c45c9451.

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+const terser = require('gulp-terser');
+const cleanCss = require('gulp-clean-css');
+const sass = require('gulp-sass');
+const { src, parallel, dest} = require('gulp'); 
+
+// FALA js & GOVUK js - copying over both if we ever want to add custom js
+const jsPath = [
+  'fala/assets-src/scripts/main.js',
+  'node_modules/govuk-frontend/dist/govuk/govuk-frontend.min.js'
+];
+
+// FALA css & GOVUK css - beacuse we import GOVUK scss into same file in order to overide/add to GOVUK classes
+const cssPath = 'fala/assets-src/sass/*.scss';
+
+// GOVUK font & images only - because we have no FALA assets anymore
+const assetPath = 'node_modules/govuk-frontend/dist/govuk/assets/**';
+
+
+ function jsTask() {
+  return src(jsPath)
+  .pipe(sourcemaps.init())
+  // name of output file
+  .pipe(concat('all.js'))
+  // a JavaScript mangler/compressor toolkit for ES6+
+  .pipe(terser())
+  .pipe(sourcemaps.write('.'))
+  // destination of new files
+  .pipe(dest('fala/assets/js'));
+}
+
+function cssTask() {
+  return src(cssPath)
+  .pipe(sourcemaps.init())
+  .pipe(sass())
+  // name of output file
+  .pipe(concat('style.css'))
+  // this minifies the CSS
+  .pipe(cleanCss())
+  .pipe(sourcemaps.write('.'))
+  // destination of new files
+  .pipe(dest('fala/assets/css'));
+}
+
+function assetTask() {
+  // { encoding: false } to copy over as binary files, GULP default is to encode into UTF:8 
+  return src(assetPath, { encoding: false })
+  .pipe(dest('fala/assets'));
+}
+
+exports.jsTask = jsTask;
+exports.cssTask = cssTask;
+exports.assetTask = assetTask;
+
+// build/execute the tasks
+exports.build = parallel(jsTask, cssTask, assetTask);
