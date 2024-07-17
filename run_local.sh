97a4057e2a211bf0cc18eb8e1cecf73233b456ba
Stephen Dicks <github@starswan.com>
2024-07-17 16:06:14 +0100

    fix issue running in docker container

--- run_local.sh
+++ run_local.sh
@@ -8,7 +8,11 @@ echo "running environment $ENVIRONMENT"
 docker-compose down --remove-orphans
 
 docker-compose up -d --build
+# This has to be run outside of the container, because the
+# entire(?) application is replaced with the local fala directory
+# when run via docker-compose
 NODE_ENV=production npm run build
+python manage.py collectstatic --noinput
 docker-compose exec webapp python3 manage.py migrate
 docker-compose logs -f webapp
 
