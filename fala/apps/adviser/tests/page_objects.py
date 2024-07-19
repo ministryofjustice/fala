2a6a5bbbbbb3385f392197fcd7d68b65b21d68c7
Stephen Dicks <github@starswan.com>
2024-07-19 09:17:02 +0100

    Added methods for results page pagination

--- fala/apps/adviser/tests/page_objects.py
+++ fala/apps/adviser/tests/page_objects.py
@@ -2,8 +2,21 @@ class ResultsPage(object):
     def __init__(self, page):
         self._page = page
 
+    @property
     def next_link(self):
-        self._page.locator('span.govuk-pagination__link-title:has-text(" Next")')
+        return self._page.locator('span.govuk-pagination__link-title:has-text(" Next")')
+
+    @property
+    def pagination_link_title(self):
+        return self._page.locator("span.govuk-pagination__link-title")
+
+    @property
+    def h1(self):
+        return self._page.locator("h1")
+
+    @property
+    def previous_link(self):
+        return self._page.locator('span.govuk-pagination__link-title:has-text(" Previous")')
 
 
 class OtherRegionPage(object):
--- fala/apps/adviser/tests/test_results_page_journeys_playwright.py
+++ fala/apps/adviser/tests/test_results_page_journeys_playwright.py
@@ -4,18 +4,9 @@ from fala.playwright.setup import PlaywrightTestSetup
 
 class ResultPageEndToEndJourneys(PlaywrightTestSetup):
 
-    def setUp(self) -> None:
-        self.page = self.browser.new_page()
-
-    def visit_valid_uk_results_page(self):
-        self.page.goto(f"{self.live_server_url}")
-        self.page.get_by_label("Postcode").fill("SE11")
-        self.page.get_by_role("button", name="Search").click()
-        expect(self.page.locator("h1")).to_have_text("Search results")
-
     def test_pagination(self):
-        self.visit_valid_uk_results_page()
-        expect(self.page.locator("span.govuk-pagination__link-title")).to_be_visible()
-        self.page.locator('span.govuk-pagination__link-title:has-text(" Next")').click()
-        expect(self.page.locator("h1")).to_have_text("Search results")
-        expect(self.page.locator('span.govuk-pagination__link-title:has-text(" Previous")')).to_be_visible()
+        page = self.visit_results_page("SE11")
+        expect(page.pagination_link_title).to_be_visible()
+        page.next_link.click()
+        expect(page.h1).to_have_text("Search results")
+        expect(page.previous_link).to_be_visible()
