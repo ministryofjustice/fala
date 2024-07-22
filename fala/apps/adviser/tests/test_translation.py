473149e1c449db8190f088984f2bfc651bcb6f83
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2024-07-22 13:28:51 +0100

    WIP: - fix breking tests

--- fala/apps/adviser/tests/test_translation.py
+++ fala/apps/adviser/tests/test_translation.py
@@ -5,6 +5,7 @@ from fala.playwright.setup import PlaywrightTestSetup
 class TranslationSelection(PlaywrightTestSetup):
 
     def test_translate_to_welsh(self):
+        page = self.browser.new_page()
         page = self.visit_search_page()
         page.language_dropdown.select_option(label="Welsh")
         expect(page.h1).to_have_text("Dewch o hyd i gynghorydd cymorth cyfreithiol neu gyfryngwr teuluol")
@@ -16,11 +17,13 @@ class TranslationSelection(PlaywrightTestSetup):
         expect(page.h1).to_have_text("Canlyniadau chwilio")
 
     def test_translate_to_irish(self):
+        page = self.browser.new_page()
         page = self.visit_search_page()
         page.language_dropdown.select_option(label="Irish Gaelic")
         expect(page.h1).to_have_text("Aimsigh comhairleoir um chúnamh dlíthiúil nó idirghabhálaí teaghlaigh")
 
     def test_translate_to_scots(self):
+        page = self.browser.new_page()
         page = self.visit_search_page()
         page.language_dropdown.select_option(label="Scots Gaelic")
         expect(page.h1).to_have_text("Lorg comhairliche taic laghail no eadar-mheadhanair teaghlaich")
