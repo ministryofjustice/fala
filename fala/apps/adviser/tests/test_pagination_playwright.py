51bfdf771d68d90077044b239d06d5889a92d600
Hettie Street <hettie.street@digital.justice.gov.uk>
2024-08-08 16:42:15 +0100

    EL-1684: Fix flakey test

--- fala/apps/adviser/tests/test_pagination_playwright.py
+++ fala/apps/adviser/tests/test_pagination_playwright.py
@@ -8,12 +8,14 @@ class PaginationResults(PlaywrightTestSetup):
         checkboxes = ["Crime"]
 
         page = self.visit_results_page("M25 3JF", checkboxes)
-        expect(page.item_from_text("1576 results in order of closeness to M25 3JF.")).to_be_visible()
+        expect(page.item_from_text("results in order of closeness to M25 3JF.")).to_be_visible()
+        first_page_result_count = page.result_count.inner_text()
         expect(page.pagination_link_title).to_be_visible()
         page.next_link.click()
-        # this tests that changing page persists the category choice by not affecting the result count
-        expect(page.item_from_text("1576 results in order of closeness to M25 3JF.")).to_be_visible()
+        # expect the result count of page 2 to match page 1.
+        # then we know the filters are still applied.
+        expect(page.result_count).to_have_text(first_page_result_count)
         page.select_page_number(4).click()
-        expect(page.item_from_text("1576 results in order of closeness to M25 3JF.")).to_be_visible()
+        expect(page.result_count).to_have_text(first_page_result_count)
         page.previous_link.click()
-        expect(page.item_from_text("1576 results in order of closeness to M25 3JF.")).to_be_visible()
+        expect(page.result_count).to_have_text(first_page_result_count)
