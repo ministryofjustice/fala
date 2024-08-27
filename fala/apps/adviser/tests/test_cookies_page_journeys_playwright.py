16fd7e4d30b9a19fe86a56f02638c159fd4e9e02
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2024-08-27 13:41:42 +0100

    EL-1550: Update cookie banner (#337)
    * EL-1550: Update Cookie Banner
    
    - remove jquery
    - remove modernizer as not used
    - add cookies.js
    - update gulp
    - add cookie component
    - add `id` and format `generic-base`
    - make sure this works when there is no JS enabled
    - refactor tests with smaller modules
    
    * Update tests suite
    
    - update circleci config to build assets for tests job
    - curl to get nvm
    - close & boot up,  to use nvm
    - add in `nvm use`
    - remove `.nvmrc` file before trying to install nvm

--- fala/apps/adviser/tests/test_cookies_page_journeys_playwright.py
+++ fala/apps/adviser/tests/test_cookies_page_journeys_playwright.py
@@ -14,73 +14,148 @@ class CookiesPageEndToEndJourneys(PlaywrightTestSetup):
 
 
 class CookiesPageEndToEndJourneysWithFreshSetUp(StaticLiveServerTestCase):
-    def test_that_cookie_has_appropriate_value(self):
+    banner_text = "Cookies on Find a legal adviser or family mediator"
+
+    def check_no_cookies(self, my_cookies):
+        assert len(my_cookies) == 0, "Cookies were found, which is surprising"
+
+    def verify_cookie_value(self, expected_value, my_cookies):
+        cookie_found = False
+        for cookie in my_cookies:
+            if cookie["value"] == expected_value:
+                cookie_found = True
+                break
+        assert cookie_found, f"No cookie with value '{expected_value}', exists"
+
+    def tear_down(self, page, context):
+        page.context.close()
+        context.browser.close()
+
+    def accept_cookie_from_banner(self, page):
+        # check that banner disappers, once cookies accepted in banner
+        # using `wait_for_selector` for this assertion to run in cricleci
+        # reload page, as cookie rejected in banner and will not appear in browser until some time
+        page.get_by_text("Accept analytics cookies", exact=True).click()
+        page.wait_for_selector(f"{self.banner_text}", state="hidden", strict=True)
+
+    def reject_cookie_from_banner(self, page):
+        page.get_by_text("Reject analytics cookies", exact=True).click()
+        page.wait_for_selector(f"{self.banner_text}", state="hidden", strict=True)
+
+    def accept_cookie_on_policy_page(self, page):
+        page.locator("#cookies_footer_link").click()
+        page.wait_for_load_state(state="domcontentloaded")
+        expect(page.locator("h1")).to_have_text("Cookies")
+        page.get_by_text("Yes", exact=True).click()
+        page.get_by_text("Save cookie settings").click()
+        page.wait_for_selector(f"{self.banner_text}", state="hidden", strict=True)
+
+    def reject_cookie_on_policy_page(self, page):
+        page.locator("#cookies_footer_link").click()
+        page.wait_for_load_state(state="domcontentloaded")
+        expect(page.locator("h1")).to_have_text("Cookies")
+        page.get_by_text("No", exact=True).click()
+        page.get_by_text("Save cookie settings").click()
+        page.wait_for_selector(f"{self.banner_text}", state="hidden", strict=True)
+
+    def test_cookie_has_appropriate_value_when_accepted_in_banner(self):
         with sync_playwright() as p:
-            browser = p.chromium.launch(headless=True)
+            browser = p.chromium.launch()
             context = browser.new_context()
             page = context.new_page()
             page.goto(f"{self.live_server_url}")
             page.wait_for_load_state(state="domcontentloaded")
 
-            banner_text = "Cookies on Find a legal adviser or family mediator"
-
-            # check there are no cookies
             my_cookies = context.cookies()
-            for cookie in my_cookies:
-                print(f"Name: {cookie['name']}, Value: {cookie['value']}")
-            assert len(my_cookies) == 0, "Cookies were found, which is surprising"
+            self.check_no_cookies(my_cookies)
 
-            # check page shows as exected with banner showing
-            expect(page.get_by_text(f"{banner_text}")).to_be_visible()
-            page.locator("#cookies_footer_link").click()
+            self.accept_cookie_from_banner(page)
+
+            my_cookies = context.cookies()
+            self.verify_cookie_value("Allowed", my_cookies)
+
+            self.tear_down(page, context)
+
+    def test_cookie_has_appropriate_value_when_rejected_in_banner(self):
+        with sync_playwright() as p:
+            browser = p.chromium.launch()
+            context = browser.new_context()
+            page = context.new_page()
+            page.goto(f"{self.live_server_url}")
             page.wait_for_load_state(state="domcontentloaded")
-            expect(page.locator("h1")).to_have_text("Cookies")
-            expect(page.get_by_text(f"{banner_text}")).to_be_visible()
-
-            # check that banner disappers, once cookies allowed in banner
-            page.get_by_text("Accept analytics cookies", exact=True).click()
-            # using `wait_for_selector` for this assertion to run in cricleci
-            page.wait_for_selector(f"{banner_text}", state="hidden")
 
             my_cookies = context.cookies()
-            cookie_found = False
-            for cookie in my_cookies:
-                print(f"Name: {cookie['name']}, Value: {cookie['value']}")
-                if cookie["value"] == "Allowed":
-                    cookie_found = True
-                    break
-            assert cookie_found, "No cookie with that value exists"
+            self.check_no_cookies(my_cookies)
 
-            # check there is a cookie with valid value, when rejected
-            page.locator("#cookies_footer_link").click()
+            self.reject_cookie_from_banner(page)
+
+            my_cookies = context.cookies()
+            self.verify_cookie_value("Rejected", my_cookies)
+
+            self.tear_down(page, context)
+
+    def test_cookie_has_appropriate_value_when_accepted_on_policy_page(self):
+        with sync_playwright() as p:
+            browser = p.chromium.launch()
+            context = browser.new_context()
+            page = context.new_page()
+            page.goto(f"{self.live_server_url}")
             page.wait_for_load_state(state="domcontentloaded")
-            page.wait_for_selector(f"{banner_text}", state="hidden")
-            page.get_by_text("No", exact=True).click()
-            page.get_by_text("Save cookie settings").click()
 
             my_cookies = context.cookies()
-            cookie_found = False
-            for cookie in my_cookies:
-                print(f"Name: {cookie['name']}, Value: {cookie['value']}")
-                if cookie["value"] == "Rejected":
-                    cookie_found = True
-                    break
-            assert cookie_found, "No cookie with that value exists"
+            self.check_no_cookies(my_cookies)
 
-            # check there is a cookie with valid value, when allowed
-            page.locator("#cookies_footer_link").click()
+            self.accept_cookie_on_policy_page(page)
+
+            my_cookies = context.cookies()
+            self.verify_cookie_value("Allowed", my_cookies)
+            expect(page.get_by_text(f"{self.banner_text}")).to_be_hidden()
+
+            self.tear_down(page, context)
+
+    def test_cookie_has_appropriate_value_when_rejected_on_policy_page(self):
+        with sync_playwright() as p:
+            browser = p.chromium.launch()
+            context = browser.new_context()
+            page = context.new_page()
+            page.goto(f"{self.live_server_url}")
             page.wait_for_load_state(state="domcontentloaded")
-            page.get_by_text("Yes", exact=True).click()
-            page.get_by_text("Save cookie settings").click()
 
             my_cookies = context.cookies()
-            cookie_found = False
-            for cookie in my_cookies:
-                print(f"Name: {cookie['name']}, Value: {cookie['value']}")
-                if cookie["value"] == "Allowed":
-                    cookie_found = True
-                    break
-            assert cookie_found, "No cookie with that value exists"
+            self.check_no_cookies(my_cookies)
 
-            context.close()
-            browser.close()
+            self.reject_cookie_on_policy_page(page)
+
+            my_cookies = context.cookies()
+            self.verify_cookie_value("Rejected", my_cookies)
+            expect(page.get_by_text(f"{self.banner_text}")).to_be_hidden()
+
+            self.tear_down(page, context)
+
+    def test_cookie_has_appropriate_value_across_pages(self):
+        with sync_playwright() as p:
+            browser = p.chromium.launch()
+            context = browser.new_context()
+            page = context.new_page()
+            page.goto(f"{self.live_server_url}")
+            page.wait_for_load_state(state="domcontentloaded")
+
+            my_cookies = context.cookies()
+            self.check_no_cookies(my_cookies)
+
+            self.accept_cookie_from_banner(page)
+
+            my_cookies = context.cookies()
+            self.verify_cookie_value("Allowed", my_cookies)
+
+            self.reject_cookie_on_policy_page(page)
+
+            my_cookies = context.cookies()
+            self.verify_cookie_value("Rejected", my_cookies)
+
+            self.accept_cookie_on_policy_page(page)
+
+            my_cookies = context.cookies()
+            self.verify_cookie_value("Allowed", my_cookies)
+
+            self.tear_down(page, context)
