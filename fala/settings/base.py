1b6103f38f6738287a4bf7d3b45dca1520ce0fa8
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2024-11-01 16:31:52 +0000

    WIP: - remove database from settings

--- fala/settings/base.py
+++ fala/settings/base.py
@@ -35,8 +35,6 @@ MANAGERS = ADMINS
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-DATABASES = {"default": {"ENGINE": "django.db.backends.postgresql"}}
-
 DEFAULT_ALLOWED_HOSTS = ".fala.dsd.io .find-legal-advice.justice.gov.uk"
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", DEFAULT_ALLOWED_HOSTS).split(" ")
 
