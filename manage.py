23023c2c3e28338387449aff936b5af066a981fa
Will Clarke <william.clarke@digital.justice.gov.uk>
2024-12-18 15:58:06 +0000

    move states into common directory

--- manage.py
+++ manage.py
@@ -5,6 +5,7 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fala.settings")
 
+    sys.path.append(os.path.join(os.path.dirname(__file__), "fala"))
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
