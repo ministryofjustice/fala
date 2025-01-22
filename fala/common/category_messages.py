1f0dad28c348e3208e55390812c978e30fc3135f
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2025-01-22 11:58:44 +0000

    - add translations for category messages

--- fala/common/category_messages.py
+++ fala/common/category_messages.py
@@ -1,9 +1,15 @@
+from django.utils.translation import gettext_lazy as _
+
 CATEGORY_MESSAGES = {
-    "hlpas": "Tell the adviser your home is at risk and you want advice through the Housing Loss Prevention Advice Service.",
-    "welfare-benefits": (
+    "hlpas": _(
+        "Tell the adviser your home is at risk and you want advice through the Housing Loss Prevention Advice Service."
+    ),
+    "welfare-benefits": _(
         "Legal aid for advice about welfare benefits is only available if you are appealing a decision made by the social security tribunal. "
         "This appeal must be in the Upper Tribunal, Court of Appeal or Supreme Court.\n\n"
         "For any other benefits issue, ask the legal adviser if you can get free legal advice or if you will have to pay for it."
     ),
-    "clinical-negligence": "Legal aid for advice about clinical negligence is usually only available if you have a child that suffered a brain injury during pregnancy, birth or the first 8 weeks of life.",
+    "clinical-negligence": _(
+        "Legal aid for advice about clinical negligence is usually only available if you have a child that suffered a brain injury during pregnancy, birth or the first 8 weeks of life."
+    ),
 }
