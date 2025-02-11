9481655aa1aa1e4b7a7cf2f67501c4cdf237058a
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2025-02-11 11:06:24 +0000

    - revert change to `urls.py` as it was only added to help with UAT

--- fala/urls.py
+++ fala/urls.py
@@ -1,7 +1,6 @@
 # coding=utf-8
 from django.conf import settings
-from django.urls import re_path, path
-from django.conf.urls.i18n import i18n_patterns
+from django.urls import re_path
 from fala.apps.adviser.views import AdviserSearchView
 from fala.apps.category_search.views import CategorySearchView
 from fala.common.results_view import ResultsView
@@ -17,22 +16,17 @@ from fala.common.compliance_views import (
 from django.views.static import serve
 
 urlpatterns = [
-    re_path(r"^robots\.txt$", RobotsTxtView.as_view(), name="robots_txt"),
-    re_path(r"^security\.txt$", SecurityTxtView.as_view(), name="security_txt"),
-    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
-]
-
-urlpatterns += i18n_patterns(
     re_path(r"^accessibility-statement$", AccessibilityView.as_view(), name="accessibility_statement"),
     re_path(r"^cookies$", CookiesView.as_view(), name="cookies"),
     re_path(r"^privacy$", PrivacyView.as_view(), name="privacy"),
-    path("", AdviserSearchView.as_view(), name="adviser"),
-    path("search", ResultsView.as_view(), name="results"),
-    path(
-        "check/<str:category>",
+    re_path(r"^$", AdviserSearchView.as_view(), name="adviser"),
+    re_path(r"^search$", ResultsView.as_view(), name="results"),
+    re_path(r"^robots\.txt$", RobotsTxtView.as_view(), name="robots_txt"),
+    re_path(r"^security\.txt$", SecurityTxtView.as_view(), name="security_txt"),
+    re_path(
+        r"^check/(?P<category>[\w-]+)$",
         CategorySearchView.as_view(),
         name="category_search",
     ),
-    path("check", CategorySearchView.as_view(), name="category_search_query"),
-    prefix_default_language=False,
-)
+    re_path(r"^check$", CategorySearchView.as_view(), name="category_search_query"),
+] + [re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT})]
