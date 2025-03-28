025665aa790d49d0f2bec651b66eee1b1c68ec31
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2025-03-28 09:51:23 +0000

    Update text, as per ticket

--- fala/common/mixin_for_views.py
+++ fala/common/mixin_for_views.py
@@ -28,7 +28,7 @@ class TranslationMixin:
         if user_language == "cy":
             return "<a class='govuk-body govuk-link' id='language_switcher_link' href='#' hreflang='en' lang='en' rel='alternate' aria-label='Change language to English'>English</a> / Cymraeg"
         else:
-            return "English / <a class='govuk-body govuk-link' id='language_switcher_link' href='#' hreflang='cy' lang='cy' rel='alternate' aria-label='Change language to English'>Cymraeg</a>"
+            return "English / <a class='govuk-body govuk-link' id='language_switcher_link' href='#' hreflang='cy' lang='cy' rel='alternate' aria-label='Change language to Welsh'>Cymraeg</a>"
 
     def language(self, request):
         user_language = request.COOKIES.get("FALA-lang", "en")
