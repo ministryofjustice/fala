b3ef887a2cf9a6304da5060f6e8c41f895f7b491
willclarke <william.clarke@digital.justice.gov.uk>
2025-05-02 12:21:57 +0100

    remove custom content for med and wb categories (#464)

--- fala/apps/category_search/tests/test_search_view.py
+++ fala/apps/category_search/tests/test_search_view.py
@@ -11,7 +11,10 @@ class SearchViewTest(TestCase):
         self.clinical_negligence_code = "med"
         self.welfare_benefits_slug = "welfare-benefits"
         self.welfare_benefits_code = "wb"
+        self.hlpas_slug = "housing-loss-prevention-advice-service"
+        self.hlpas_code = "hlpas"
         self.clinical_negligence_url = reverse("category_search", kwargs={"category": "clinical-negligence"})
+        self.hlpas_url = reverse("category_search", kwargs={"category": "housing-loss-prevention-advice-service"})
         self.welfare_benefits_url = reverse("category_search", kwargs={"category": "welfare-benefits"})
 
         settings.FEATURE_FLAG_SINGLE_CATEGORY_SEARCH_FORM = True
@@ -25,6 +28,10 @@ class SearchViewTest(TestCase):
         self.assertRedirects(response, reverse("adviser"))
 
     def test_redirects_to_correct_category_url_welfare_benefits(self):
+        response = self.client.get(reverse("category_search_query") + f"?categories={self.welfare_benefits_code}")
+        self.assertRedirects(response, self.welfare_benefits_url)
+
+    def test_redirects_to_correct_category_url_hlpas(self):
         response = self.client.get(reverse("category_search_query") + f"?categories={self.welfare_benefits_code}")
         self.assertRedirects(response, self.welfare_benefits_url)
 
@@ -50,16 +57,30 @@ class SearchViewTest(TestCase):
         self.assertNotEqual(response.url, self.welfare_benefits_url)
         self.assertNotIn(f"/{self.welfare_benefits_slug}", response.url)
 
+    def test_displays_search_form_hlpas(self):
+        response = self.client.get(self.hlpas_url)
+        self.assertEqual(response.status_code, 200)
+        content = response.content.decode("utf-8")
+
+        self.assertRegex(
+            content,
+            re.compile(r"Find a legal aid adviser for\s+the Housing Loss Prevention Advice Service", re.IGNORECASE),
+        )
+        self.assertRegex(
+            content,
+            re.compile(
+                r"Tell the adviser your home is at risk and you want advice through the Housing Loss Prevention Advice Service.",
+                re.IGNORECASE,
+            ),
+        )
+        self.assertRegex(content, re.compile(r'<input type="hidden" name="categories" value="hlpas">', re.IGNORECASE))
+
     def test_displays_search_form_clinical_negligence(self):
         response = self.client.get(self.clinical_negligence_url)
         self.assertEqual(response.status_code, 200)
         content = response.content.decode("utf-8")
 
         self.assertRegex(content, re.compile(r"Find a legal aid adviser for\s+clinical negligence", re.IGNORECASE))
-        self.assertRegex(
-            content,
-            re.compile(r"Legal aid for advice about clinical negligence is usually only available", re.IGNORECASE),
-        )
         self.assertRegex(content, re.compile(r'<input type="hidden" name="categories" value="med">', re.IGNORECASE))
 
     def test_displays_search_form_welfare_benefits(self):
@@ -68,13 +89,6 @@ class SearchViewTest(TestCase):
         content = response.content.decode("utf-8")
 
         self.assertRegex(content, re.compile(r"Find a legal aid adviser for\s+welfare benefits", re.IGNORECASE))
-        self.assertRegex(
-            content,
-            re.compile(
-                r"Legal aid for advice about welfare benefits is only available if you are appealing a decision made by the social security tribunal.",
-                re.IGNORECASE,
-            ),
-        )
         self.assertRegex(content, re.compile(r'<input type="hidden" name="categories" value="wb">', re.IGNORECASE))
         self.assertRegex(
             content,
--- fala/common/category_messages.py
+++ fala/common/category_messages.py
@@ -4,12 +4,4 @@ CATEGORY_MESSAGES = {
     "housing-loss-prevention-advice-service": _(
         "Tell the adviser your home is at risk and you want advice through the Housing Loss Prevention Advice Service."
     ),
-    "welfare-benefits": _(
-        "Legal aid for advice about welfare benefits is only available if you are appealing a decision made by the social security tribunal. "
-        "This appeal must be in the Upper Tribunal, Court of Appeal or Supreme Court.\n\n"
-        "For any other benefits issue, ask the legal adviser if you can get free legal advice or if you will have to pay for it."
-    ),
-    "clinical-negligence": _(
-        "Legal aid for advice about clinical negligence is usually only available if you have a child that suffered a brain injury during pregnancy, birth or the first 8 weeks of life."
-    ),
 }
