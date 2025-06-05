55934444586cd77927e6db9254ea343af88872b0
said-moj <45761276+said-moj@users.noreply.github.com>
2025-06-05 15:42:31 +0100

    Add rebrand toggle flag for non production sites

--- fala/apps/adviser/context_processors.py
+++ fala/apps/adviser/context_processors.py
@@ -5,4 +5,8 @@ def current_environment(request):
     return {"ENVIRONMENT": settings.ENVIRONMENT}
 
 def govuk_rebrand(request):
-    return {"GOVUK_REBRAND_ENABLED": settings.GOVUK_REBRAND_ENABLED}
+    govuk_rebrand_enabled = settings.GOVUK_REBRAND_ENABLED
+    if not govuk_rebrand_enabled and settings.ENVIRONMENT != "production":
+        govuk_rebrand_enabled = request.GET.get("govuk_rebrand_enabled", "false").lower() == "true"
+
+    return {"GOVUK_REBRAND_ENABLED": govuk_rebrand_enabled}
