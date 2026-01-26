b58b8ea18562c49f53e5a2af5c804b29223fc0fb
Charles Pinnock <chalston.pinnock@justice.gov.uk>
2026-01-26 17:11:05 +0000

    testing pipeline

--- fala/common/tests/playwright/test_pagination_playwright.py
+++ fala/common/tests/playwright/test_pagination_playwright.py
@@ -5,9 +5,10 @@ from fala.common.test_utils.playwright.setup import PlaywrightTestSetup
 class PaginationResults(PlaywrightTestSetup):
 
     def test_results_pagination(self):
-        checkboxes = ["Crime"]
+        checkboxes = ["Debt"]
 
         page = self.visit_results_page(postcode="M25 3JF", checkbox_labels=checkboxes)
+        print("this is the page details", page)
         expect(page.item_from_text("results in order of closeness to M25 3JF")).to_be_visible()
         first_page_result_count = page.result_count.inner_text()
         expect(page.pagination_link_title).to_be_visible()
