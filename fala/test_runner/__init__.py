3a34b3f4926d495b3d241d063e125254b56be0e1
charles <chalston.pinnock@justice.gov.uk>
2026-01-27 12:48:58 +0000

    Update README and Replace External Fala API Call with Mock in Pipeline (#498)
    * updates for local dev experience, bash script check the system before start installing
    
    * remove the hardcode version for better reader visbility
    
    * changing back to python 3.12
    
    * update readme to tell developer what requires for local run & change docker compose to find assets
    
    * testing pipeline
    
    * testing pipeline
    
    * testing
    
    * testing pipeline now
    
    * testing pipeline
    
    * testing the pipeline
    
    * testing the pipeline
    
    * update on the testing
    
    * Try to get unit test to pass using mock laalaa calls
    
    * some refactoring of the code
    
    * testing pipeline again
    
    * saving the latest changes
    
    * fixing lala api call for mocking data
    
    * testing pipeline
    
    * testing the pipeline works
    
    * Restructure test data
    
    * Fix mock laalaa_search
    
    * remove api_v1 file from lala no longer needed
    
    * remove imports no longer needed
    
    * remove imports no longer needed
    
    ---------
    
    Co-authored-by: said-moj <45761276+said-moj@users.noreply.github.com>

---
+++ fala/test_runner/__init__.py
@@ -0,0 +1,59 @@
+import json
+import os
+from django.test.runner import DiscoverRunner
+from unittest import mock
+from django.utils.translation import gettext_lazy as _
+
+
+def _context_manager(file_path: str) -> json:
+    path = os.path.dirname(os.path.realpath(__file__))
+    path = os.path.join(path, "mock_laalaa_test_data", file_path)
+    with open(path, "r") as file:
+        return json.load(file)
+
+
+def get_categories():
+
+    items = _context_manager("mock_api_categories.json")
+    return {item["code"].lower(): _(item["name"]) for item in items}
+
+
+def get_results_for_charles():
+    return _context_manager("mock_lala_company_charles.json")
+
+
+def laalaa_search(**kwargs):
+
+    organisation_name = kwargs.get("organisation_name")
+    if organisation_name in ["test", "burns"]:
+        return _context_manager("mock_empty_result.json")
+
+    if organisation_name == "charles":
+        return _context_manager("mock_laalaa_company_charles.json")
+
+    page = kwargs.get("page", 0)
+    if isinstance(page, int) and page > 4565:
+        return json.loads("""{"error": "Invalid page."}""", strict=False)
+
+    return _context_manager("mock_lala_search_all.json")
+
+
+class FalaTestRunner(DiscoverRunner):
+    def setup_test_environment(self, **kwargs):
+        # This runs BEFORE the apps are fully ready for testing
+        self.laala_api_client_patcher = mock.patch(
+            "cla_common.laalaa.LaalaaProviderCategoriesApiClient.get_categories"
+        )
+        laala_api_client_mock = self.laala_api_client_patcher.start()
+        laala_api_client_mock.return_value = get_categories()
+
+        self.laalaa_search_patcher = mock.patch("fala.apps.laalaa.api.laalaa_search")
+        laalaa_search_mock = self.laalaa_search_patcher.start()
+        laalaa_search_mock.side_effect = laalaa_search
+
+        super().setup_test_environment(**kwargs)
+
+    def teardown_test_environment(self, **kwargs):
+        super().teardown_test_environment(**kwargs)
+        self.laala_api_client_patcher.stop()
+        self.laalaa_search_patcher.stop()
